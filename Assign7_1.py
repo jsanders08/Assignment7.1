d5487b5c55d78fd3c1e268ed404dd63542673658
jsanders08 <82988477+jsanders08@users.noreply.github.com>
2021-04-25 16:46:42 -0700

    Update Assign7_1.py

--- Assign7_1.py
+++ Assign7_1.py
@@ -4,4 +4,7 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/jsanders08/Assignment7.1.git
+$ git config --global user.name "jsanders08"
+$ git config --global user.email "jeremiah.sanders.33@outlook.com
 git push -u origin main
+.gitignore __pycache__/
